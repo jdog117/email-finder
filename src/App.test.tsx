cbae697edfb14c1c2df990cb6029e896e85a37d9
jdog117 <jspcasey@gmail.com>
2024-02-29 14:24:29 -0800

    test shenanagins

---
+++ src/App.test.tsx
@@ -0,0 +1,20 @@
+// import { render, screen } from "@testing-library/react";
+// import "@testing-library/jest-dom"; // Add this line
+// import { describe, expect, it } from "vitest";
+// import App from "./App"; // Import the App component
+
+// describe("App", () => {
+//     it("Renders Find Email", () => {
+//         //ARRANGE
+//         render(<App />);
+//         //ACT
+//         //EXPECT
+//         expect(
+//             screen.getByRole("heading", {
+//                 level: 1,
+//             })
+//         ).toHaveTextContent("Email Finder");
+//     });
+// });
+
+//TOO MUCH TROUBLE RIGHT NOW :(
--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     test: {
         globals: true,
         environment: "jsdom",
-        setupFiles: ["./src/setup.ts"],
+        setupFiles: ["./src/setupTests.ts"],
         pool: "forks",
     },
 });
