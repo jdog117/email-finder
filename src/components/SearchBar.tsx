7c58a8d1b36dd076e2f56aedd8b4628891e1ab02
jdog117 <jspcasey@gmail.com>
2024-03-06 12:57:56 -0800

    global style changes

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
-function Bar() {
+function SearchBar() {
     return (
         <div className="flex w-full max-w-sm items-center space-x-2">
             <Input type="domain" placeholder="website" />
-            <Button type="submit">Submit</Button>
+            <div className="flex-none h-10 border border-input bg-background px-3 py-2 ring-offset-background disabled:cursor-not-allowed disabled:opacity-50">
+                @
+            </div>
+            <Input type="employee" placeholder="name" />
+            <Button type="submit">Find</Button>
         </div>
     );
 }
 
-export default Bar;
+export default SearchBar;
