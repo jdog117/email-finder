7f3af8e2d24b964225284154286cf85df61d0759
jdog117 <jspcasey@gmail.com>
2024-03-11 12:17:22 -0700

    vercel api setup

---
+++ api/index.ts
@@ -0,0 +1,27 @@
+const express = require("express");
+import cors from "cors";
+import { verifyEmail } from "../src/lib/verifyEmail.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/verifyEmail", async (req, res) => {
+    const { website, personName } = req.query;
+
+    try {
+        const verifiedEmail = await verifyEmail(website, personName);
+        res.status(200).send(verifiedEmail);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
+module.exports = app;
+
+// node server.js TO RUN
+// http://localhost:3001/verifyEmail?website=example.com&personName=john.doe
