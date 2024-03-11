a9ebb47520ea26a691569a36ef31e379ea84c5b8
jdog117 <jspcasey@gmail.com>
2024-03-11 13:34:38 -0700

    import fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { WrappedApp } from "./App.tsx";
+import { WrappedApp } from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
